cf95c8dc06d659b47f922443f3964c193f2f4528
Nitzan Dunsky <nitzandunsky@mail.tau.ac.il>
2022-11-17 18:14:32 +0200

    revert cd execution, fix jobs print

--- src/handleInternal.c
+++ src/handleInternal.c
@@ -8,24 +8,12 @@
 #include "generalFunctions.h"
 #include "handleExternal.h"
 
-void execute_cd(char **cmd_args)
+void execute_cd(char *dest_dir)
 {
-  // validate cd command
-  int cmd_args_len = 0;
-  for (int i=0; i<INPUT_BUFFER_LEN; ++i){
-    if (cmd_args[i]== NULL){
-      cmd_args_len = i;
-      break;
-    }
-  }
-  int missing_dir_arg = (cmd_args_len == 1);
-  int wrong_flags = ((cmd_args_len == 3) && (strcmp(cmd_args[1],"-L")!=0) && (strcmp(cmd_args[1],"-P")!=0));
-  int too_many_arguments = (cmd_args_len > 3); //TODO find out if 'directory not exist' considered in valid command
-
-  if (missing_dir_arg || wrong_flags || too_many_arguments ) {
+    if (dest_dir== NULL) {
     printf("hw1shell: invalid command\n");
   } else {
-    if (chdir(cmd_args[1]) != 0) {
+    if (chdir(dest_dir) != 0) {
       system_call_err("cd");
      
     }
@@ -38,7 +26,6 @@ void execute_jobs(child_process *child_list, int *current_childs_count)
   is_any_child_finished(child_list, current_childs_count);
 
   // print all the background jobs that are still running
-  printf("PID\tCommand\n"); //TODO check if we need to keep
   for (int i = 0; i < *current_childs_count; i++)
   {
     if (child_list[i].pid != 0)
@@ -54,7 +41,7 @@ void execute_exit()
   // wait for all the background jobs to finish
   int status;
   pid_t pid;
-  while ((pid = waitpid(-1, &status, 0)) > 0) //TODO handle error of waitpid
+  while ((pid = waitpid(-1, &status, 0)) > 0)
   {
     printf("hw1shell: pid %d finished\n", pid);
   }
@@ -68,7 +55,7 @@ void execute_internal(char **cmd_args, int cmd_index, child_process *child_list,
     switch(cmd_index)
     {
         case 0:
-            execute_cd (cmd_args);
+            execute_cd (cmd_args[1]);
             break;
             
         case 1:
